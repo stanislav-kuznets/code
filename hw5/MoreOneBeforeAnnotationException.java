f7afa54032c3f856dafa81fc89a8231926f76820
stanislav-kuznets <36127370+stanislav-kuznets@users.noreply.github.com>
2018-03-25 20:04:25 +0300

    Delete MoreOneBeforeAnnotationException.java

--- hw5/MoreOneBeforeAnnotationException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.skuznets.Exceptions;
-
-public class MoreOneBeforeAnnotationException extends Exception {
-    public MoreOneBeforeAnnotationException() {
-        super("Больше одной аннотации Before.");
-    }
-}
